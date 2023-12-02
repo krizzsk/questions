d497086c02891bf85a4eb639c9a2d4b0d1f55036
Joel Verghese <51381507+krizzsk@users.noreply.github.com>
2023-12-02 13:09:48 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -3,13 +3,14 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from google.oauth2 import service_account
 import gspread
-import threading  # Import the threading module
+import threading
+import logging
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # Set up Google Sheets API
-scopes = ["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"]
+scopes = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 credentials = service_account.Credentials.from_service_account_file(
     "/root/credentials.json", scopes=scopes
 )
@@ -18,10 +19,13 @@ gc = gspread.authorize(credentials)
 
 # Open the Google Sheet by title and specify the worksheet
 spreadsheet = gc.open("SEDATA")
-worksheet = spreadsheet.get_worksheet(0)  # Choose the appropriate worksheet
+worksheet = spreadsheet.get_worksheet(0)
 
-# Create a thread lock
-lock = threading.Lock()
+# Create a semaphore with a limit of 1 to control access to the count update process
+semaphore = threading.Semaphore(1)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/count', methods=['POST'])
 def update_topic_count():
@@ -29,7 +33,6 @@ def update_topic_count():
     topic = data['topic']
     count = data['count']
 
-    # Define the topic names and their corresponding cell addresses
     topics = {
         "DevSecOps": "A",
         "ETAP": "B",
@@ -39,22 +42,24 @@ def update_topic_count():
         "Better Luck Next Time": "F",
     }
 
-    # Check if the topic is in the predefined topics
     if topic in topics:
         col_letter = topics[topic]
-        col_index = gspread.utils.a1_to_rowcol(f"{col_letter}1")[1]  # Get the column index from the column letter
-        
-        with lock:
+        col_index = gspread.utils.a1_to_rowcol(f"{col_letter}1")[1]
+
+        with semaphore:
             cell_value = worksheet.cell(1, col_index).value
             current_count_cell = worksheet.cell(2, col_index)
-            
+
             if current_count_cell.value:
-                current_count = int(current_count_cell.value)  # Get the count cell below the topic cell
+                current_count = int(current_count_cell.value)
             else:
                 current_count = 0
 
             new_count = current_count + count
-            worksheet.update_cell(2, col_index, new_count)  # Update the count in the corresponding cell
+            worksheet.update_cell(2, col_index, new_count)
+
+            logging.info(f"Topic: {topic}, Old Count: {current_count}, New Count: {new_count}")
+
     else:
         return jsonify({'error': 'Topic not found'})
 
